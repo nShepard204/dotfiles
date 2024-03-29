dea1ebf3ff5b08f27d39079b4aaa44f80e4d35cf
nShepard204 <nShepard204@gmail.com>
2024-03-29 13:26:25 -0400

    Organized the repo a wittle

--- scripts/update-configs.sh
+++ scripts/update-configs.sh
@@ -2,7 +2,7 @@
 
 # Kitty & Picom are managed by i3's config. Doesn't need to be handled here.
 # I3
-I3_CFG_SRC="$HOME/repos/dotfiles/i3/config"
+I3_CFG_SRC="$HOME/repos/dotfiles/configs/i3.conf"
 I3_CFG_DEST="$HOME/.config/i3/config"
 if [ -n "$(diff "$I3_CFG_SRC" "$I3_CFG_DEST")" ]; then
 	echo "I3 Config Updated. Copying Changes."
@@ -12,7 +12,7 @@ else
 fi
 
 # Dunst
-DUNST_CFG_SRC="$HOME/repos/dotfiles/dunstrc"
+DUNST_CFG_SRC="$HOME/repos/dotfiles/configs/dunst.conf"
 DUNST_CFG_DEST="/usr/local/etc/xdg/dunst/dunstrc"
 if [ -n "$(diff "$DUNST_CFG_SRC" "$DUNST_CFG_DEST")" ]; then
 	echo "Dunst Config Updated. Copying Changes."
